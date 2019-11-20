9f839cd3b5006acab00c1f293b8f8b5fd2391e2f
Rodrigo Simon <rodrigo.simon85@gmail.com>
2019-11-20 20:21:25 -0300

    yelp mvp pretty much done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'restaurants', to: 'restaurants#index', as: :restaurants
+
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  # post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Amarelinho', address: 'Glória', phone_number: '2558 3502', category: 'italian')
+Restaurant.create(name: 'Maya', address: 'Vila Aymoré', phone_number: '2222 4444', category: 'belgian')
+Restaurant.create(name: 'Le Ble Noir', address: 'Xavier da Silveira, 19', phone_number: '2558 3502', category: 'french')
+Restaurant.create(name: 'Meng Yuan', address: ' Uruguai, 262', phone_number: '2570 9234', category: 'japanese')
+Restaurant.create(name: 'Xiaoxian', address: 'Uruguai, 131 ', phone_number: '2238 8088', category: 'chinese')
+Restaurant.create(name: 'Belga Brasserie', address: 'Andradas, 129', phone_number: '3333 2222', category: 'belgian')
